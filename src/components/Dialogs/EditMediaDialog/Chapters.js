0047506e3b6ed3ae3c760e9e99b7d5424b58aac9
Renée Kooi <renee@kooi.me>
2020-08-22 19:04:07 +0200

    Display the chapter duration

--- src/components/Dialogs/EditMediaDialog/Chapters.js
+++ src/components/Dialogs/EditMediaDialog/Chapters.js
@@ -2,9 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Radio from '@material-ui/core/Radio';
+import formatDuration from 'format-duration';
+
+function ChapterItem({
+  active, start, end, title, onClick,
+}) {
+  const duration = end - start;
+
+  return (
+    <ListItem
+      className="ChapterItem"
+      button
+      onClick={onClick}
+    >
+      <ListItemIcon>
+        <Radio
+          checked={active}
+          tabIndex={-1}
+        />
+      </ListItemIcon>
+      <ListItemText>
+        {title}
+        <span className="ChapterItem-duration">
+          {formatDuration(duration * 1000)}
+        </span>
+      </ListItemText>
+    </ListItem>
+  );
+}
+
+ChapterItem.propTypes = {
+  active: PropTypes.bool.isRequired,
+  start: PropTypes.number.isRequired,
+  end: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 function Chapters({
   className,
@@ -14,21 +50,16 @@ function Chapters({
   onChange,
 }) {
   return (
-    <List className={className} dense disablePadding>
+    <List className={className} disablePadding>
       {available.map((chapter) => (
-        <ListItem
+        <ChapterItem
           key={chapter.start}
-          button
           onClick={() => onChange(chapter)}
-        >
-          <ListItemIcon>
-            <Radio
-              checked={chapter.start === start && chapter.end === end}
-              tabIndex={-1}
-            />
-          </ListItemIcon>
-          <ListItemText primary={chapter.title} />
-        </ListItem>
+          active={chapter.start === start && chapter.end === end}
+          start={chapter.start}
+          end={chapter.end}
+          title={chapter.title}
+        />
       ))}
     </List>
   );
