d14580b94264090460f51792d9e4ade916b4198c
Renée Kooi <renee@kooi.me>
2020-08-23 15:27:22 +0200

    use a <Select /> for chapters

--- src/components/Dialogs/EditMediaDialog/Chapters.js
+++ src/components/Dialogs/EditMediaDialog/Chapters.js
@@ -1,46 +1,10 @@
+import clsx from 'clsx';
 import React from 'react';
 import PropTypes from 'prop-types';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Radio from '@material-ui/core/Radio';
+import { useTranslator } from '@u-wave/react-translate';
+import MenuItem from '@material-ui/core/MenuItem';
 import formatDuration from 'format-duration';
-
-function ChapterItem({
-  active, start, end, title, onClick,
-}) {
-  const duration = end - start;
-
-  return (
-    <ListItem
-      className="ChapterItem"
-      button
-      onClick={onClick}
-    >
-      <ListItemIcon>
-        <Radio
-          checked={active}
-          tabIndex={-1}
-        />
-      </ListItemIcon>
-      <ListItemText>
-        {title}
-        <span className="ChapterItem-duration">
-          {formatDuration(duration * 1000)}
-        </span>
-      </ListItemText>
-    </ListItem>
-  );
-}
-
-ChapterItem.propTypes = {
-  active: PropTypes.bool.isRequired,
-  start: PropTypes.number.isRequired,
-  end: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
+import Select from '../../Form/Select';
 
 function Chapters({
   className,
@@ -49,19 +13,37 @@ function Chapters({
   end,
   onChange,
 }) {
+  const { t } = useTranslator();
+  const handleChange = (event) => {
+    const newIndex = event.target.value;
+    onChange(available[newIndex]);
+  };
+
+  const selectedIndex = available.findIndex(
+    (chapter) => chapter.start === start && chapter.end === end,
+  );
+
   return (
-    <List className={className} disablePadding>
-      {available.map((chapter) => (
-        <ChapterItem
-          key={chapter.start}
-          onClick={() => onChange(chapter)}
-          active={chapter.start === start && chapter.end === end}
-          start={chapter.start}
-          end={chapter.end}
-          title={chapter.title}
-        />
+    <Select
+      className={clsx('Chapters', className)}
+      classes={{
+        select: 'Chapters-select',
+      }}
+      onChange={handleChange}
+      value={selectedIndex}
+    >
+      <MenuItem disabled value={-1}>
+        {t('dialogs.editMedia.chapterCount', { count: available.length })}
+      </MenuItem>
+      {available.map((chapter, index) => (
+        <MenuItem className="ChapterItem" value={index}>
+          {chapter.title}
+          <span className="ChapterItem-duration">
+            {formatDuration((chapter.end - chapter.start) * 1000)}
+          </span>
+        </MenuItem>
       ))}
-    </List>
+    </Select>
   );
 }
 
